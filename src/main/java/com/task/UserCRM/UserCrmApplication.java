bc0a9457d6693ba89bfc348f7cd5894a9b9a2d6d
Peace <55354072+nickimilie@users.noreply.github.com>
2024-05-28 09:36:57 +0200

    initialization

---
+++ src/main/java/com/task/UserCRM/UserCrmApplication.java
@@ -0,0 +1,13 @@
+package com.task.UserCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/UserCRM/UserCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.UserCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
