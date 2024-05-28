ed5425150290583630479a7f68a346648c70e467
Peace <55354072+nickimilie@users.noreply.github.com>
2024-05-28 13:09:46 +0200

    repository test

---
+++ src/test/java/com/task/UserCRM/repository/UserRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.task.UserCRM.repository;
+
+import com.task.UserCRM.Enum.Gender;
+import com.task.UserCRM.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryTest(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @AfterEach
+    void cleanDatabase() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @Sql(scripts = {"classpath:sql/initiate_user.sql"})
+    public void get_user(){
+
+        User testUser = userRepository.findById(1).get();
+
+        assertThat(testUser.getUsername()).isEqualTo("TestUser");
+    }
+
+    @Test
+    public void create_user(){
+
+        User user = new User();
+
+        user.setUsername("test");
+        user.setAge(19);
+        user.setGender(Gender.MALE.name());
+
+        userRepository.save(user);
+
+        User testUser = userRepository.findById(1).orElse(new User());
+
+        assertThat(testUser.getUsername()).isEqualTo(user.getUsername());
+    }
+
+
+    @Test
+    @Sql(scripts = {"classpath:sql/initiate_user.sql"})
+    public void delete_user(){
+
+        userRepository.deleteById(1);
+
+        User testUser = userRepository.findById(1).orElse(new User());
+
+        assertThat(testUser.getUsername()).isEqualTo(null);
+    }
+
+}
---
+++ src/test/resources/sql/initiate_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO users(id, username, gender, age) VALUES(1, 'TestUser', 'MALE', 19);
